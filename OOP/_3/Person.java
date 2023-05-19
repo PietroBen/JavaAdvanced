0eb003d89240de168b9d571b56031f8136a260a7
PietroBen <pietrobenedicenti@gmail.com>
2023-05-19 13:47:34 +0200

    Esercizio su Override e HashCode()

--- OOP/_3/Person.java
+++ OOP/_3/Person.java
@@ -1,2 +1,29 @@
-package JavaAdvanced.OOP._3;public class Person {
+package JavaAdvanced.OOP._3;
+
+public class Person {
+
+    String name;
+    int age;
+    String address;
+
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name = "+ name +
+                ", age = " + age +
+                ", address = " + address +
+                "}";
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() +
+                age +
+                address.hashCode();
+    }
 }
